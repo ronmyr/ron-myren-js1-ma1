037b58f5b112b3e0ce6c8267b9dcfe660ddb20af
ronmyr <ron.myren@gmail.com>
2022-11-13 18:45:09 +0100

    Question 4
    Finished question 4

--- js/script.js
+++ js/script.js
@@ -21,4 +21,15 @@ const cat = {
 }
 
 cat.complain();
+
 // Question 2
+var heading = document.querySelector("h3");
+heading.innerHTML = "Updated title";
+
+//Question 3
+heading.style.cssText = "font-size: 2em";
+
+//Question 4
+heading.classList.add("subheading");
+
+//Question 5
